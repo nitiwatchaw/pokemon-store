414617302b089cd1d76f5db09089d0596d92dc71
nitiwat <nitiwatcmu@gmail.com>
2023-11-23 12:45:22 +0700

    search

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Card/Card'
 import GridLoader from "react-spinners/GridLoader";
 import './Store.css'
+import { MdOutlineLocationSearching } from "react-icons/md";
 const Store = ({ loading, poke, prev, next, prevUrl, nextUrl, setCart, cart }) => {
+    const [search, setSearch] = useState("")
+
 
     useEffect(() => {
         localStorage.setItem("cart", JSON.stringify(cart))
     }, [cart])
     return (
         <div className='store-container'>
-            <div className="btn-wrap">
-                <button disabled={!prevUrl} onClick={prev}>Prev</button>
-                <button disabled={!nextUrl} onClick={next}>Next</button>
+            <div className="header">
+                <div className="btn-wrap">
+                    <button disabled={!prevUrl} onClick={prev}>Prev</button>
+                    <button disabled={!nextUrl} onClick={next}>Next</button>
+                </div>
+                <div className="search">
+                    <input type="search" placeholder='Poke Search' value={search} onChange={(e) => setSearch(e.target.value)} />
+                    <MdOutlineLocationSearching onClick={() => { setSearch("") }} style={{ cursor: "pointer" }} />
+                </div>
             </div>
             {loading ?
                 <GridLoader
@@ -25,9 +34,13 @@ const Store = ({ loading, poke, prev, next, prevUrl, nextUrl, setCart, cart }) =
 
                 <>
                     <div className="card-container">
-                        {poke.map((pokemon) => {
+                        {poke.filter(val => val.name.toLowerCase().includes(search.toLocaleLowerCase())).map((pokemon) => {
                             return <Card key={pokemon.id} pokemon={pokemon} setCart={setCart} />
+
                         })}
+                        {poke.filter((val) => val.name.toLowerCase().includes(search.toLowerCase())).length === 0 && (
+                            <p>Can't find any Pokémon.</p>
+                        )}
                     </div>
                     <div className="btn-wrap">
                         <button disabled={!prevUrl} onClick={prev}>Prev</button>
